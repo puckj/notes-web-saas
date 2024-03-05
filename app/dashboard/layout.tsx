be16b2aa00e72be2de4399357544e211ef5952bd
Pongsapuck_Dev <pongsapuck.j@gmail.com>
2024-03-05 15:51:32 +0700

    sync kinde(auth) with supabase(prisma)

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,10 +1,54 @@
 import DashboardNav from "@/components/DashboardNav";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { redirect } from "next/navigation";
+import prisma from "../lib/db";
 
-const DashboardLayout = ({
+const getData = async ({
+  email,
+  id,
+  firstName,
+  lastName,
+  profileImage,
+}: getDataProps) => {
+  const user = await prisma.user.findUnique({
+    where: {
+      id: id,
+    },
+    select: {
+      id: true,
+      stripeCustomerId: true,
+    },
+  });
+  if (!user) {
+    const name = `${firstName ?? ""} ${lastName ?? ""}`;
+    await prisma.user.create({
+      data: {
+        id: id,
+        email: email,
+        name: name,
+      },
+    });
+  }
+};
+
+const DashboardLayout = async ({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) => {
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
+  if (!user) {
+    return redirect("/");
+  } else {
+    await getData({
+      email: user.email as string,
+      firstName: user.given_name as string,
+      lastName: user.family_name as string,
+      id: user.id as string,
+      profileImage: user.picture as string,
+    });
+  }
   return (
     <div className="flex flex-col space-y-6 mt-10">
       <div className="container grid flex-1 gap-12 md:grid-cols-[200px_1fr]">
--- types/index.ts
+++ types/index.ts
@@ -1,5 +1,13 @@
 type UserNavProps = {
-    name: string,
-    email: string,
+    name: string
+    email: string
     image: string
+}
+
+type getDataProps = {
+    email: string
+    id: string
+    firstName: string | undefined | null
+    lastName: string | undefined | null
+    profileImage: string | undefined | null
 }
